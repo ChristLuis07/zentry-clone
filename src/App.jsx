c0f0463d1306b0ae84087f2ca39db5677ecf7ec0
Christian <christianluisginting@gmail.com>
2025-04-16 03:47:34 +0700

    "finishing project"

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
+import Features from './components/Features'
+import Story from './components/Story'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -9,8 +13,11 @@ const App = () => {
     <main className='relative min-h-screen w-screen overflow-x-hidden'> 
       <Navbar />
       <Hero />
-
       <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   )
 }
